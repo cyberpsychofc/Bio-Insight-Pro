aaf99af42be73ef36308499e9ee0a87fb48cf519
PiyushDhamecha454 <piyushdhamecha327@gmail.com>
2025-01-22 16:32:17 +0530

    v1.0.1 : Minor Fixes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,8 +2,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import Navbar from './components/Navbar.jsx'
 import Footer from './components/Footer.jsx'
-import Form from './components/~Form.jsx'
-import TogglePlug from './components/~TogglePlug.jsx'
+import Form from './components/Form.jsx'
+import TogglePlug from './components/TogglePlug.jsx'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
